133516da863daf8ca7a83c93c7a6d35e0fbf20aa
Luah Jun Yang <e0725519@u.nus.edu>
2023-10-13 17:03:36 +0800

    Added FilterCommand class

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.tag.Tag;
+
+import java.util.List;
+
+public class FilterCommand extends Command {
+
+    public static final String COMMAND_WORD = "filter";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Filters for all the persons that have target tag being filtered. \n"
+            + "Parameters: TAG (must be a valid tag from the predefined list) \n"
+            + "Example: " + COMMAND_WORD + " friend";
+
+    private final List<Tag> tags;
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        return null;
+    }
+}
