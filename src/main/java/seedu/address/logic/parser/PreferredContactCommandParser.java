a8f851c9f5f7fe3bd9252e740327d3f221b619d2
zhanyang01 <ngzhanyang01@gmail.com>
2023-10-17 14:43:34 +0800

    Fix checkstyles

--- src/main/java/seedu/address/logic/parser/PreferredContactCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PreferredContactCommandParser.java
@@ -19,7 +19,7 @@ public class PreferredContactCommandParser implements Parser<PreferredContactCom
      * Parses the given {@code String} of arguments in the context of the
      * EditCommand
      * and returns an EditCommand object for execution.
-     * 
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public PreferredContactCommand parse(String args) throws ParseException {
