ae229fb0ab2d8674083b5809d7f0cfb0e6934a5c
Teng Xin Zhuan <>
2023-10-27 23:17:02 +0800

    add tests

--- src/test/java/seedu/address/logic/commands/ChangePasswordCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ChangePasswordCommandTest.java
@@ -2,9 +2,17 @@ package seedu.address.logic.commands;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.storage.PasswordManager;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 public class ChangePasswordCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void different_passwordManager() {
@@ -25,6 +33,15 @@ public class ChangePasswordCommandTest {
         assert(firstChangePasswordCommand.equals(secondChangePasswordCommand));
     }
 
+
+    @Test
+    public void null_passwordManager() {
+        PasswordManager passwordManager = new PasswordManager();
+        String password = passwordManager.getPassword();
+        ChangePasswordCommand ChangePasswordCommand = new ChangePasswordCommand(password, password);
+        assert(!ChangePasswordCommand.equals(null));
+    }
+
     @Test
     public void same_passwordManager_toString() {
         PasswordManager passwordManager = new PasswordManager();
@@ -32,6 +49,15 @@ public class ChangePasswordCommandTest {
         ChangePasswordCommand firstChangePasswordCommand = new ChangePasswordCommand(password, password);
         ChangePasswordCommand secondChangePasswordCommand = new ChangePasswordCommand(password, password);
         assert(firstChangePasswordCommand.toString().equals(secondChangePasswordCommand.toString()));
+    }
+
+    @Test
+    public void passwordManager_execute_success() {
+        PasswordManager passwordManager = new PasswordManager();
+        String password = passwordManager.getPassword();
+        ChangePasswordCommand changePasswordCommand = new ChangePasswordCommand(password, password);
+        String expectedMessage = ChangePasswordCommand.CHANGE_PASSWORD_SUCCESS + password;
+        assertCommandSuccess(changePasswordCommand, model, expectedMessage, model);
     }
 
 }
