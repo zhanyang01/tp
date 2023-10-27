5812035141b90c442b7e960de39633c93913f39f
Teng Xin Zhuan <>
2023-10-27 11:33:21 +0800

    remove unused imports

--- src/test/java/seedu/address/storage/PasswordManagerTest.java
+++ src/test/java/seedu/address/storage/PasswordManagerTest.java
@@ -3,17 +3,6 @@ package seedu.address.storage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
-import seedu.address.commons.core.GuiSettings;
-import seedu.address.model.AddressBook;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.UserPrefs;
-
-import java.nio.file.Path;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 public class PasswordManagerTest {
 
