bf2b510cb82a310c480557c8a7052ae3a0f12ebf
Luah Jun Yang <e0725519@u.nus.edu>
2023-10-29 19:46:10 +0800

    Fixed filter function to show error if user enters command without the prefix tag (t/)

--- src/main/java/seedu/address/model/person/FilterContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/FilterContainsKeywordsPredicate.java
@@ -1,7 +1,9 @@
 package seedu.address.model.person;
 
 import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.tag.Tag;
@@ -18,16 +20,38 @@ public class FilterContainsKeywordsPredicate implements Predicate<Person> {
 
     @Override
     public boolean test(Person person) {
-        for (Tag tag : person.getTags()) {
-            String tagName = tag.getTagName(); // Replace with the actual method to get the tag name
+        for (String keyword : keywords) {
+            System.out.println("Current keyword: " + keyword);
+            boolean keywordFound = false;
+            for (Tag tag : person.getTags()) {
+                String tagName = tag.getTagName();
+                System.out.println("Current tagName: " + tagName);
+                if (keyword.equalsIgnoreCase(tagName)) {
+                    keywordFound = true;
+                    System.out.println("Match! ");
+                    break;
+                } else {
+                    keywordFound = false;
+                }
+            }
+            if (!keywordFound) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**for (Tag tag : person.getTags()) {
+            String tagName = tag.getTagName();
             for (String keyword : keywords) {
                 if (keyword.equalsIgnoreCase(tagName)) {
-                    return true; // Match found, exit early
+                    return true;
                 }
             }
         }
-        return false; // No match found after checking all tags and keywords
-    }
+        return false;**/
+
+
 
     @Override
     public boolean equals(Object other) {
