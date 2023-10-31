0fb19b1f2869eeb4a8c04ac0db030d3aefa21805
zhanyang01 <ngzhanyang01@gmail.com>
2023-10-31 11:06:08 +0800

    Add preferred meeting region attribute.

---
+++ src/main/java/seedu/address/logic/commands/GroupMeetingCommand.java
@@ -0,0 +1,28 @@
+/*
+ * package seedu.address.logic.commands;
+ * 
+ * import static java.util.Objects.requireNonNull;
+ * 
+ * import seedu.address.commons.util.ToStringBuilder;
+ * import seedu.address.logic.Messages;
+ * import seedu.address.logic.commands.exceptions.CommandException;
+ * import seedu.address.model.Model;
+ * import seedu.address.model.person.FilterContainsKeywordsPredicate;
+ * 
+ * public class GroupMeetingCommand extends Command {
+ * 
+ * public static final String COMMAND_WORD = "groupmeeting";
+ * public static final String MESSAGE_USAGE = COMMAND_WORD +
+ * ": Finds all persons whose names contain any of "
+ * +
+ * "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+ * + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+ * + "Example: " + COMMAND_WORD + " north south east west central";
+ * 
+ * private final GroupMeetingContainsKeywordsPredicate predicate;
+ * 
+ * public GroupMeetingCommand(GroupMeetingContainsKeywordsPredicate predicate) {
+ * this.predicate = predicate;
+ * }
+ * }
+ */
