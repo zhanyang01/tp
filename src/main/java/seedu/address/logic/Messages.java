c5c0b3c834898bfb1c63cb9d8d5a6a9fdd2c712a
zhanyang01 <ngzhanyang01@gmail.com>
2023-10-31 17:48:18 +0800

    Fix checkstyles

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -12,43 +12,42 @@ import seedu.address.model.person.Person;
  */
 public class Messages {
 
-        public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
-        public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-        public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-        public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-        public static final String MESSAGE_DUPLICATE_FIELDS = "Multiple values specified for the following single-valued field(s): ";
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_DUPLICATE_FIELDS = "Multiple values specified for the following single-valued field(s): ";
 
-        /**
-         * Returns an error message indicating the duplicate prefixes.
-         */
-        public static String getErrorMessageForDuplicatePrefixes(Prefix... duplicatePrefixes) {
-                assert duplicatePrefixes.length > 0;
+    /**
+     * Returns an error message indicating the duplicate prefixes.
+     */
+    public static String getErrorMessageForDuplicatePrefixes(Prefix... duplicatePrefixes) {
+        assert duplicatePrefixes.length > 0;
 
-                Set<String> duplicateFields = Stream.of(duplicatePrefixes).map(Prefix::toString)
-                                .collect(Collectors.toSet());
+        Set<String> duplicateFields = Stream.of(duplicatePrefixes).map(Prefix::toString)
+                .collect(Collectors.toSet());
 
-                return MESSAGE_DUPLICATE_FIELDS + String.join(" ", duplicateFields);
-        }
+        return MESSAGE_DUPLICATE_FIELDS + String.join(" ", duplicateFields);
+    }
 
-        /**
-         * Formats the {@code person} for display to the user.
-         */
-        public static String format(Person person) {
-                final StringBuilder builder = new StringBuilder();
-                builder.append(person.getName())
-                                .append("; Phone: ")
-                                .append(person.getPhone())
-                                .append("; Email: ")
-                                .append(person.getEmail())
-                                .append("; Address: ")
-                                .append(person.getAddress())
-                                .append(", PreferredContact: ")
-                                .append(person.getPreferredContact())
-                                .append(", PreferredMeetingRegion: ")
-                                .append(person.getPreferredMeetingRegion())
-                                .append("; Tags: ");
-                person.getTags().forEach(builder::append);
-                return builder.toString();
-        }
-
+    /**
+     * Formats the {@code person} for display to the user.
+     */
+    public static String format(Person person) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(person.getName())
+                .append("; Phone: ")
+                .append(person.getPhone())
+                .append("; Email: ")
+                .append(person.getEmail())
+                .append("; Address: ")
+                .append(person.getAddress())
+                .append(", PreferredContact: ")
+                .append(person.getPreferredContact())
+                .append(", PreferredMeetingRegion: ")
+                .append(person.getPreferredMeetingRegion())
+                .append("; Tags: ");
+        person.getTags().forEach(builder::append);
+        return builder.toString();
+    }
 }
