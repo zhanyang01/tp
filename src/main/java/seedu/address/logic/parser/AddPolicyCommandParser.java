e05ee761403d82a8fc03cb17fd89ff4127d5411f
johnnythesnake12 <e0773684@u.nus.edu>
2023-10-31 23:28:49 +0800

    Policies can now be added temporarily and displayed on Ui, however, still unable to store permanently

--- src/main/java/seedu/address/logic/parser/AddPolicyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddPolicyCommandParser.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.*;
 
@@ -15,25 +16,30 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddPolicyCommand;
 import seedu.address.logic.commands.AddPolicyCommand.AddPolicyDescriptor;
 import seedu.address.logic.commands.AddTagCommand;
+import seedu.address.logic.commands.DeleteTagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.policy.Policy;
 import seedu.address.model.tag.Tag;
 
 public class AddPolicyCommandParser implements Parser<AddPolicyCommand> {
-    private static final Pattern ADD_POLICY_COMMAND_FORMAT = Pattern.compile("addPolicy\\s+(\\d+)\\s+pn/([^\\s]+)\\s+pd/([^\\s]+)\\s+pv/([\\d.]+)\\s+psd/(\\S+)\\s+ped/(\\S+)");
+
     @Override
     public AddPolicyCommand parse(String args) throws ParseException {
-        final Matcher matcher = ADD_POLICY_COMMAND_FORMAT.matcher(args.trim());
-        if (!matcher.matches()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPolicyCommand.MESSAGE_USAGE));
-        }
-        final Index index = ParserUtil.parseIndex(matcher.group(1));
-        final String policyName = matcher.group(2);
-        final String description = matcher.group(3);
-        final String policyValue = matcher.group(4);
-        final String startDate = matcher.group(5);
-        final String endDate = matcher.group(6);
+        requireNonNull(args);
+        Index index;
 
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_POLICY_NAME, PREFIX_POLICY_DESCRIPTION, PREFIX_POLICY_VALUE, PREFIX_POLICY_START_DATE, PREFIX_POLICY_END_DATE);
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPolicyCommand.MESSAGE_USAGE), pe);
+        }
+
+        final String policyName = argMultimap.getValue(PREFIX_POLICY_NAME).get();
+        final String description = argMultimap.getValue(PREFIX_POLICY_DESCRIPTION).get();
+        final String policyValue = argMultimap.getValue(PREFIX_POLICY_VALUE).get();
+        final String startDate = argMultimap.getValue(PREFIX_POLICY_START_DATE).get();
+        final String endDate = argMultimap.getValue(PREFIX_POLICY_END_DATE).get();
 
         AddPolicyDescriptor addPolicyDescriptor = new AddPolicyDescriptor();
         parsePolicyForEdit(policyName, description, policyValue, startDate, endDate).ifPresent(addPolicyDescriptor::setPolicies);
@@ -47,11 +53,10 @@ public class AddPolicyCommandParser implements Parser<AddPolicyCommand> {
         assert startDate != null;
         assert endDate != null;
 
-
         return Optional.of(ParserUtil.parsePolicy(policyName, description, policyValue, startDate, endDate));
     }
 
-}
 
+}
 
 
