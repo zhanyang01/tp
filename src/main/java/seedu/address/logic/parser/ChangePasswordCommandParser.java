1b31aec5526bf20d6b0c8cb99c50244ed667f5c5
Luah Jun Yang <e0725519@u.nus.edu>
2023-11-07 12:51:40 +0800

    Resolved issue #187, #186

--- src/main/java/seedu/address/logic/parser/ChangePasswordCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ChangePasswordCommandParser.java
@@ -4,8 +4,11 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NEW_PASSWORD;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_OLD_PASSWORD;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_INPUT;
 
 import java.util.stream.Stream;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import seedu.address.logic.commands.ChangePasswordCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -30,9 +33,14 @@ public class ChangePasswordCommandParser implements Parser<ChangePasswordCommand
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_OLD_PASSWORD, PREFIX_NEW_PASSWORD);
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+$");
         String oldPassword = argMultimap.getValue(PREFIX_OLD_PASSWORD).get();
         String newPassword = argMultimap.getValue(PREFIX_NEW_PASSWORD).get();
+        Matcher matcher = pattern.matcher(newPassword);
 
+        if (!matcher.matches()) {
+            throw new ParseException(MESSAGE_INVALID_INPUT);
+        }
         return new ChangePasswordCommand(oldPassword, newPassword);
     }
 
