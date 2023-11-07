8e2e544613f55a507b85bd75f73002d1e2e8c12e
Luah Jun Yang <e0725519@u.nus.edu>
2023-11-07 19:19:46 +0800

    Added individual import statements for messages.

--- src/main/java/seedu/address/logic/parser/AddPolicyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddPolicyCommandParser.java
@@ -2,7 +2,8 @@ package seedu.address.logic.parser;
 
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.Messages.*;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_ALPHANUMERIC_INPUT;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_DESCRIPTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_END_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_NAME;
