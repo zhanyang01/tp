069473ec92aa6675ba709cb0e044fd889c802f4b
johnnythesnake12 <e0773684@u.nus.edu>
2023-11-10 16:45:24 +0800

    Invalid dates now throw a parse exception

--- src/main/java/seedu/address/logic/parser/DateValidator.java
+++ src/main/java/seedu/address/logic/parser/DateValidator.java
@@ -4,6 +4,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+
+/**
+ * Checks if a date is valid or real
+ */
 public class DateValidator {
     private static final String DATE_REGEX = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$";
 
@@ -27,20 +31,20 @@ public class DateValidator {
         int year = localDate.getYear();
 
         switch (month) {
-            case 2:
-                // Check for leap year
-                if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-                    return dayOfMonth <= 29;
-                } else {
-                    return dayOfMonth <= 28;
-                }
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                return dayOfMonth <= 30;
-            default:
-                return dayOfMonth <= 31;
+        case 2:
+            // Check for leap year
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                return dayOfMonth <= 29;
+            } else {
+                return dayOfMonth <= 28;
+            }
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return dayOfMonth <= 30;
+        default:
+            return dayOfMonth <= 31;
         }
     }
 }
